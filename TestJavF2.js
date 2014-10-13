f795c1831cea57186b8e3e8a093e10e4be1e1e86
Ajith <a.padmakumar@accenture.com>
2014-10-13 13:23:30 +0530

    New-Update

--- TestJavF2.js
+++ TestJavF2.js
@@ -1,7 +1,7 @@
 package de.vogella.git.first;
 
-public class GitTest76 {
+public class GitTest56 {
   public static void main(String[] args) {
-    System.out.println("Git is version control");
+    System.out.println("Git is VSS");
   }
 } 
